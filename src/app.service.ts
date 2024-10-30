11581b3e9471033fa04089848d9333be7dd734f2
Piotr Jura <jura.piotrek@gmail.com>
2024-10-30 18:48:52 +0100

    008-exercise-1-solution

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { DummyService } from './dummy/dummy.service';
+import { LoggerService } from './logger/logger.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly dummyService: DummyService) {}
+  constructor(
+    private readonly dummyService: DummyService,
+    private readonly loggerSerivce: LoggerService,
+  ) {}
 
   getHello(): string {
-    return `Hello World! ${this.dummyService.work()}`;
+    return this.loggerSerivce.log(`Hello World! ${this.dummyService.work()}`);
   }
 }
