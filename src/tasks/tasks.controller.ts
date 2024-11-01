ee38efb9afb3ade940bdd4dd33932f677e4aaa66
Piotr Jura <jura.piotrek@gmail.com>
2024-11-01 19:04:14 +0100

    014-route-params

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
   @Get()
   public findAll(): string[] {
     return ['A', 'B'];
+  }
+
+  @Get('/:id')
+  public findOne(@Param('id') id: string): string {
+    return `The number is ${id}`;
   }
 }
