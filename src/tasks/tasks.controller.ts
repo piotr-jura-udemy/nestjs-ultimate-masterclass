b6d6a76f4b74aba4ff0a70ec3fe5dcfa0359fd9b
Piotr Jura <jura.piotrek@gmail.com>
2024-11-03 16:25:50 +0100

    017-fetching-data-in-service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,17 +1,18 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { ITask } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  public findAll(): string[] {
-    return ['A', 'B'];
+  public findAll(): ITask[] {
+    return this.tasksService.findAll();
   }
 
   @Get('/:id')
-  public findOne(@Param('id') id: string): string {
-    return `The number is ${id}`;
+  public findOne(@Param('id') id: string): ITask {
+    return this.tasksService.findOne(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,4 +4,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TasksService {
   private tasks: ITask[] = [];
+
+  findAll(): ITask[] {
+    return this.tasks;
+  }
+
+  findOne(id: string): ITask | undefined {
+    return this.tasks.find((task) => task.id === id);
+  }
 }
