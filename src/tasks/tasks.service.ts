1be72221c6067d762fbcb9dff3cbb20713fc3bd0
Piotr Jura <jura.piotrek@gmail.com>
2024-11-06 18:16:42 +0100

    024-patch-update-task-status

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,21 +1,21 @@
 import { randomUUID } from 'node:crypto';
 import { CreateTaskDto } from './create-task.dto';
-import { ITask } from './task.model';
+import { ITask, TaskStatus } from './task.model';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TasksService {
   private tasks: ITask[] = [];
 
-  findAll(): ITask[] {
+  public findAll(): ITask[] {
     return this.tasks;
   }
 
-  findOne(id: string): ITask | undefined {
+  public findOne(id: string): ITask | undefined {
     return this.tasks.find((task) => task.id === id);
   }
 
-  create(createTaskDto: CreateTaskDto): ITask {
+  public create(createTaskDto: CreateTaskDto): ITask {
     const task: ITask = {
       id: randomUUID(),
       ...createTaskDto,
