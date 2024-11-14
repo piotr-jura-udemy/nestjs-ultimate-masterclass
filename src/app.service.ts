1efcc6d80a28f3c48c595e38c3b9da6beb6d4fc4
Piotr Jura <jura.piotrek@gmail.com>
2024-11-14 16:44:14 +0100

    037-typing-config

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { DummyService } from './dummy/dummy.service';
 import { LoggerService } from './logger/logger.service';
 import { ConfigService } from '@nestjs/config';
+import { ConfigType } from './config/config.types';
+import { AppConfig } from './config/app.config';
 
 @Injectable()
 export class AppService {
   constructor(
     private readonly dummyService: DummyService,
     private readonly loggerSerivce: LoggerService,
-    private readonly configService: ConfigService,
+    private readonly configService: ConfigService<ConfigType>,
   ) {}
 
   getHello(): string {
-    const prefix = this.configService.get<string>('app.messagePrefix');
+    const prefix = this.configService.get<AppConfig>('app')?.messagePrefix;
     return this.loggerSerivce.log(
       `${prefix} Hello World! ${this.dummyService.work()}`,
     );
