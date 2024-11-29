9e15d23dde81808882eb81ed2c424fd898732a52
Piotr Jura <jura.piotrek@gmail.com>
2024-11-29 19:30:07 +0100

    072-query-builder

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -24,23 +24,23 @@ export class TasksService {
     filters: FindTaskParams,
     pagination: PaginationParams,
   ): Promise<[Task[], number]> {
-    const where: FindOptionsWhere<Task> = {};
+    const query = this.tasksRepository
+      .createQueryBuilder('task')
+      .leftJoinAndSelect('task.labels', 'labels');
 
     if (filters.status) {
-      where.status = filters.status;
+      query.andWhere('task.status = :status', { status: filters.status });
     }
 
     if (filters.search?.trim()) {
-      where.title = Like(`%${filters.search}%`);
-      where.description = Like(`%${filters.search}%`);
+      query.andWhere(
+        '(task.title ILIKE :search OR task.description ILIKE :search)',
+        { search: `%${filters.search}%` },
+      );
     }
 
-    return await this.tasksRepository.findAndCount({
-      where,
-      relations: ['labels'],
-      skip: pagination.offset,
-      take: pagination.limit,
-    });
+    query.skip(pagination.offset).take(pagination.limit);
+    return query.getManyAndCount();
   }
 
   public async findOne(id: string): Promise<Task | null> {
