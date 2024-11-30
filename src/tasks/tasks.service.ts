e7a77c9b834fd5adbec342009c4e155f15cd76eb
Piotr Jura <jura.piotrek@gmail.com>
2024-11-30 13:45:34 +0100

    074-subquery

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -40,7 +40,14 @@ export class TasksService {
     }
 
     if (filters.labels?.length) {
-      query.andWhere('labels.name IN (:...names)', { names: filters.labels });
+      const subQuery = query
+        .subQuery()
+        .select('labels.taskId')
+        .from('task_label', 'labels')
+        .where('labels.name IN (:...names)', { names: filters.labels })
+        .getQuery();
+
+      query.andWhere(`task.id IN ${subQuery}`);
     }
 
     query.skip(pagination.offset).take(pagination.limit);
