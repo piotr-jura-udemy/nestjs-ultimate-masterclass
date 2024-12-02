48a7617cc31e22cdfde9c67888e18e26f18d8233
Piotr Jura <jura.piotrek@gmail.com>
2024-12-02 17:18:07 +0100

    077-users-module

---
+++ src/config/auth.config.ts
@@ -0,0 +1,22 @@
+// jwt:
+//   secret
+
+import { registerAs } from '@nestjs/config';
+
+//   expiresIn
+export interface AuthConfig {
+  jwt: {
+    secret: string;
+    expiresIn: string;
+  };
+}
+
+export const authConfig = registerAs(
+  'auth',
+  (): AuthConfig => ({
+    jwt: {
+      secret: process.env.JWT_TOKEN as string,
+      expiresIn: process.env.JWT_EXPIRES_IN ?? '60m',
+    },
+  }),
+);
