9f918afe54e3423f531402957c47547cffda29c9
Piotr Jura <jura.piotrek@gmail.com>
2024-12-02 19:21:38 +0100

    078-users-module-done

---
+++ src/users/users.module.ts
@@ -0,0 +1,23 @@
+import { AuthConfig } from './../config/auth.config';
+import { TypedConfigService } from './../config/typed-config.service';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: TypedConfigService) => ({
+        secret: config.get<AuthConfig>('auth')?.jwt.secret,
+        signOptions: {
+          expiresIn: config.get<AuthConfig>('auth')?.jwt.expiresIn,
+        },
+      }),
+    }),
+  ],
+})
+export class UsersModule {}
