b7cf48279ba09c4ed05d7919c9f81a0168333b8b
Piotr Jura <jura.piotrek@gmail.com>
2024-12-03 18:46:07 +0100

    079-create-user-dto

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,12 +4,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.registerAsync({
+      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: TypedConfigService) => ({
         secret: config.get<AuthConfig>('auth')?.jwt.secret,
