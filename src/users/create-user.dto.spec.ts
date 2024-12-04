0b8929caad9a93d08d326d2b08c90915c7f71e6b
Piotr Jura <jura.piotrek@gmail.com>
2024-12-04 15:05:21 +0100

    082-testing-failure

--- src/users/create-user.dto.spec.ts
+++ src/users/create-user.dto.spec.ts
@@ -1,21 +1,33 @@
 import { validate } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
+import exp from 'constants';
 
 describe('CreateUserDto', () => {
-  it('2+2=4', () => {
-    expect(2).toBe(2); // Assertion
-  });
+  let dto = new CreateUserDto();
 
-  it('should validate complete valid data', async () => {
-    // 3xA
-    // Arrange
-    const dto = new CreateUserDto();
+  beforeEach(() => {
+    dto = new CreateUserDto();
     dto.email = 'test@test.com';
     dto.name = 'Piotr';
     dto.password = '123456';
+  });
+
+  it('should validate complete valid data', async () => {
+    // Arrange
     // Act
     const errors = await validate(dto);
     // Assert
     expect(errors.length).toBe(0);
+  });
+
+  it('should fail on invalid email', async () => {
+    // Arrange
+    dto.email = 'test';
+    // Act
+    const errors = await validate(dto);
+    // Assert
+    expect(errors.length).toBeGreaterThan(0);
+    expect(errors[0].property).toBe('email');
+    expect(errors[0].constraints).toHaveProperty('isEmail');
   });
 });
